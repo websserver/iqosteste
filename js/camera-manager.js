b78667892f96a782819569e7110d29cce36de54c
websserver <websserver2@gmail.com>
2025-04-02 18:49:50 +0100

    Melhorado gerenciamento de permissões da câmara para iOS

--- js/camera-manager.js
+++ js/camera-manager.js
@@ -2,12 +2,18 @@
 const CameraManager = {
     // Chave para o Session Storage
     CAMERA_STATE_KEY: 'camera_permission_state',
+    IS_IOS: /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream,
 
     // Inicializa o gerenciador
     init: function() {
-        // Verifica se já temos permissão
+        // No iOS, não tentamos solicitar permissão automaticamente
+        if (this.IS_IOS) {
+            console.log('Dispositivo iOS detectado - aguardando interação do usuário');
+            return;
+        }
+
+        // Para outros dispositivos, verifica se já temos permissão
         if (!this.hasStoredPermission()) {
-            // Se não tiver, solicita permissão
             this.requestCameraPermission();
         }
     },
@@ -20,9 +26,15 @@ const CameraManager = {
     // Solicita permissão da câmera
     requestCameraPermission: async function() {
         try {
-            const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+            const stream = await navigator.mediaDevices.getUserMedia({ 
+                video: {
+                    facingMode: 'environment'
+                }
+            });
+            
             // Armazena o estado da permissão
             sessionStorage.setItem(this.CAMERA_STATE_KEY, 'granted');
+            
             // Para a stream para não consumir recursos
             stream.getTracks().forEach(track => track.stop());
         } catch (error) {
